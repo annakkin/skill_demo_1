a0d37fa012b53398fffa9f929894c6719fdc2eec
annakkin <515579592@qq.com>
2022-02-03 21:57:08 -0800

    skill_demo and test

---
+++ skill_demo.java
@@ -0,0 +1,5 @@
+public class skill_demo {
+    public static int multiply(int x, int y) {
+        return x*y;
+    }
+}
---
+++ skill_demo_test.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class skill_demo_test {
+    @Test
+    public void testMultiply() {
+        assertEquals(4, skill_demo.multiply(2, 3));
+    }
+
+}
